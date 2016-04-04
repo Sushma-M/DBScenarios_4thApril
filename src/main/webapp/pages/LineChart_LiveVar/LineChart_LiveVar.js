afb23847516e9d76a17f610fff8ef6d71f008783
sushma pkrm null <sushmapkrm@gmail.com>
2016-04-04 06:09:23 +0000

    New files added - [LineChart_LiveVar.css,hrdb_procedure.json,Acc_ReadOnlyDataGrid.html,DepartmentService.java,page.min.html,EditableDataGrid.variables.json,UserServiceImpl.java,VacationDao.java,ReadOnlyDataGrid.html,PagewithLiveForm.variables.json,Acc_ReadOnlyDataGrid.js,ReadOnlyDataGrid.js,LineChartwithAggrgtn.variables.json,pagewithLiveList.html,pagewithLiveList.variables.json,LineChartwithAggrgtn.js,hrdb_vk.hbm.xml,page.min.html,HrdbProcedureExecutorServiceImpl.java,PagewithLivegrid.variables.json,page.min.html,HrdbQueryExecutorService.java,LineChart_LiveVar.js,Acc_ReadOnlyDataGrid.css,DepartmentController.java,hrdb.properties,hrdb.log,hrdb_published_dataModel.json,PagewithCharts.js,PagewithLivegrid.css,Acc_ReadOnlyDataGrid.variables.json,PagewithLiveForm.js,hrdb.spring.xml,hrdb_query.json,LineChartwithAggrgtn.css,UserController.java,EmployeeDao.java,Tabs_ReadOnlyGrid.html,EditableDataGrid.css,page.min.html,QueryExecutionController.java,PagewithCharts.variables.json,PagewithLiveForm.html,EmployeeService.java,page.min.html,ReadOnlyDataGrid.css,UserDao.java,Tabs_ReadOnlyGrid.variables.json,page.min.html,User.java,pagewithLiveList.js,DepartmentServiceImpl.java,UserService.java,PagewithLivegrid.js,PagewithLivegrid.html,page.min.html,hrdb-queries.hbm.xml,EditableDataGrid.html,VacationController.java,PagewithLiveForm.css,Tabs_ReadOnlyGrid.js,hrdb-procedures.mappings.json,DepartmentDao.java,ProcedureExecutionController.java,db-connection-settings.json,EditableDataGrid.js,VacationServiceImpl.java,LineChartwithAggrgtn.html,pagewithLiveList.css,page.min.html,ReadOnlyDataGrid.variables.json,Vacation.java,EmployeeServiceImpl.java,Tabs_ReadOnlyGrid.css,LineChart_LiveVar.variables.json,hrdb_API.json,PagewithCharts.css,Department.java,servicedef.xml,Employee.java,page.min.html,HrdbProcedureExecutorService.java,PagewithCharts.html,hrdb.script,page.min.html,LineChart_LiveVar.html,hrdb.properties,HrdbQueryExecutorServiceImpl.java,VacationService.java,EmployeeController.java,LineChart_LiveVar,Acc_ReadOnlyDataGrid,EditableDataGrid,ReadOnlyDataGrid,PagewithLiveForm,LineChartwithAggrgtn,pagewithLiveList,PagewithLivegrid,PagewithCharts,Tabs_ReadOnlyGrid] Files modified - [page.min.html,Main.html,footer.variables.json,user-web.xml,page.min.html,rightnav.variables.json,Common.variables.json,.wmproject.properties,app.variables.json,topnav.variables.json,leftnav.variables.json,Login.html,page.min.html,leftnav.html,pages-config.json,header.html,project-services.xml,page.min.html,page.min.html,page.min.html,Login.variables.json,Main.variables.json,production.properties,header.variables.json,pom.xml,index.html,page.min.html,page.min.html,development.properties] Refactored files - [] Fixed bugs - [] Removed files - []

---
+++ src/main/webapp/pages/LineChart_LiveVar/LineChart_LiveVar.js
@@ -0,0 +1,32 @@
+Application.$controller("LineChart_LiveVarPageController", ["$scope", function ($scope) {
+    "use strict";
+
+    /* perform any action on widgets/variables within this block */
+    $scope.onPageReady = function () {
+        /*
+         * variables can be accessed through '$scope.Variables' property here
+         * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
+         * $scope.Variables.loggedInUser.getData()
+         *
+         * widgets can be accessed through '$scope.Widgets' property here
+         * e.g. to get value of text widget named 'username' use following script
+         * '$scope.Widgets.username.datavalue'
+         */
+    };
+
+}]);
+
+
+Application.$controller("dialog1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("dialog2Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
